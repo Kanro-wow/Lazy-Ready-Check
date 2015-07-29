77dd0bdba6564f19eb2c9650db62caf65bf41963
Thijs ter Velde <thijsiee.tv@gmail.com>
2015-07-29 03:37:06 +0200

    adding a timer on the ready check button, starting with localization, now accepting with random decimal number, changed enable/disable to on/off

--- LazyReadycheck.lua
+++ LazyReadycheck.lua
@@ -1,60 +1,69 @@
 local f = CreateFrame("Frame")
-local countdownF = CreateFrame("Frame","LCR_Countdown")
-countdownF.counter = CreateTexture(nil, "ARTWORK")
 f:RegisterEvent("PLAYER_LOGIN")
-local enabled = false
 
-local orange = string.format("|cff%02x%02x%02x",255,165,0).."LazyReadyCheck: |r"
-if LazyReadyCheck == nil then LazyReadyCheck = { ["onLoad"] = false, } end
+local stringLRC = [[|cffFFA500LazyReadyCheck:|r ]]
+if LazyReadyCheck == nil then LazyReadyCheck = { ["login"] = false, } end
 
 local function registerEvents(enable)
 	if enable then
 		f:RegisterEvent("READY_CHECK")
-		print(orange.."Turned |cff33CC33on|r! Will accept readychecks. Type /lrc for options")
-		enabled = true
+		print(stringLRC.."Turned |cff33CC33on|r! Will accept readychecks. Type /lrc for options")
 	else
 		f:UnregisterEvent("READY_CHECK")
-		print(orange.."Turned |cffFF0000off|r! Will ignore readychecks. Type /lrc for options")
-		enabled = false
+		print(stringLRC.."Turned |cffFF0000off|r! Will ignore readychecks. Type /lrc for options")
+	end
+end
+
+local function runTimer(time)
+	if time then
+		local acceptTime = time
+		local elapsedTime = 0
+		local textTime = false
+		f:SetScript("OnUpdate", function(self,elapsed)
+			elapsedTime = elapsedTime + elapsed
+			if elapsedTime > acceptTime then
+				local test = READY
+				ReadyCheckFrameYesButton:SetText(test)
+				ReadyCheckFrameYesButton:Click()
+				return runTimer(false)
+			end
+			local time = string.format("%.1f", acceptTime - elapsedTime)
+			if textTime ~= time then
+				ReadyCheckFrameYesButton:SetText(READY.." ("..time..")")
+				textTime = time
+			end
+		end)
+	else
+		f:SetScript("OnUpdate", nil)
 	end
 end
 
 f:SetScript("OnEvent", function(self,event,...)
 	if event == "READY_CHECK" then
-		local time = math.random(1.0,5.0)
-		print(orange.."Accepting in",time,"seconds")
-		C_Timer.After(time, function()
-			ReadyCheckFrameYesButton:Click()
-		end)
+		local acceptTime = math.random(100,500)/100
+		runTimer(acceptTime)
+		print(stringLRC.."Accepting in",math.ceil(acceptTime),"~ seconds")
 	elseif event == "PLAYER_LOGIN" then
-		countdownF:SetSize(100,100)
-		countdownF.counter:SetNormalFontObject(_G["GameFontNormal"])
-
-
-		registerEvents(LazyReadyCheck.onLoad)
+		registerEvents(LazyReadyCheck.login)
 	end
 end)
 
 SLASH_LAZYREADYCHECK1 = "/lrc"
 SLASH_LAZYREADYCHECK2 = "/lazyreadycheck"
 SlashCmdList["LAZYREADYCHECK"] = function(msg, editbox)
-	-- if string.len(msg) == 0 then
-	-- 	registerEvents(not enabled)
-	-- else
-	if msg == "enable" then
+	-- enable/disable deprecated
+	if msg == "on" or msg == "enable" then
 		registerEvents(true)
-	elseif msg == "disable" then
+	elseif msg == "off" or msg == "disable" then
 		registerEvents(false)
-	elseif msg == "onload true" then
-		LazyReadyCheck.onLoad = true
-		print(orange.."|cff33CC33Turned on during login!|r! When logging in, addon will accept ready checks!")
-	elseif msg == "onload false" then
-		LazyReadyCheck.onLoad = false
-		print(orange.."|cffFF0000Turned off during login|r! When logging in, addon will ignore ready checks!")
+	elseif msg == "login on" then
+		LazyReadyCheck.login = true
+		print(stringLRC.."|cff33CC33Turned on during login!|r! When logging in, addon will accept ready checks!")
+	elseif msg == "login off" then
+		LazyReadyCheck.login = false
+		print(stringLRC.."|cffFF0000Turned off during login|r! When logging in, addon will ignore ready checks!")
 	else
-		print(orange.."/lrc - will toggle the addon on or off")
-		print(orange.."/lrc enable - autoaccept readychecks")
-		print(orange.."/lrc disable - ignore readychecks")
-		print(orange.."/lrc onload true/false- turn on Lazy Ready Check on logging in")
+		print(stringLRC.."/lrc on/off - autoaccept or ignore readychecks")
+		print(stringLRC.."/lrc login on/off- turn on Lazy Ready Check when logging in")
 	end
 end

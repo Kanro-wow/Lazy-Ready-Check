2501ec94d0eb825d0c519abaeb14058eb3ca9569
Thijs ter Velde <thijsiee.tv@gmail.com>
2015-08-01 04:52:09 +0200

    final 1.2. moved strings to become localized, table atm. cleaned and minified for release

--- LazyReadycheck.lua
+++ LazyReadycheck.lua
@@ -1,70 +1,60 @@
-READY_CHECK_WAITING_TEXTURE = "Interface\\RaidFrame\\ReadyCheck-Waiting";
-READY_CHECK_READY_TEXTURE = "Interface\\RaidFrame\\ReadyCheck-Ready";
-READY_CHECK_NOT_READY_TEXTURE = "Interface\\RaidFrame\\ReadyCheck-NotReady";
-READY_CHECK_AFK_TEXTURE = "Interface\\RaidFrame\\ReadyCheck-NotReady";
-
-local f = CreateFrame("Frame")
-f:RegisterEvent("PLAYER_LOGIN")
-local enabled = nil
-
-local stringLRC = [[|cffFFA500LazyReadyCheck:|r ]]
 if LazyReadyCheck == nil then LazyReadyCheck = { ["login"] = false, } end
+local strings = {
+	["on"] = [[Turned |cff33CC33on|r! Will accept ready checks.]],
+	["off"] = [[Turned |cffFF0000off|r! Will ignore ready checks.]],
+	["options"] = [[Type /LRC for options]],
+	["lrc"] = [[|cffFFA500LazyReadyCheck:|r]],
+	["onLogin"] = [[|cff33CC33Turned on after login!|r! When logging in, addon will accept ready checks!]],
+	["offLogin"] = [[|cffFF0000Turned off after login|r! When logging in, addon will ignore ready checks!]],
+	["onOff"] = [[/LRC on/off - autoaccept or ignore ready checks]],
+	["onOffLogin"] = [[/LRC login on/off- turn on Lazy Ready Check when logging in]],
+}
 
-local function enableAddon(enable)
-	enabled = enable
-	if enable then
-		print(stringLRC..[[Turned |cff33CC33on|r! Will accept ready checks.]])
-		ReadyCheckFrameYesButton.elapsed = math.random(100,500)/10
-	else
-		print(stringLRC..[[Turned |cffFF0000off|r! Will ignore ready checks.]])
+SLASH_LAZYREADYCHECK1 = "/lrc"
+SLASH_LAZYREADYCHECK2 = "/lazyreadycheck"
+SlashCmdList["LAZYREADYCHECK"] = function(msg, editbox)
+	if msg == "on" or msg == "enable" then
+		print(strings["lrc"],strings["on"])
+		ReadyCheckFrameYesButton.elapsed = math.random(10,50)/10
+		enabled = true
+	elseif msg == "off" or msg == "disable" then
+		print(strings["lrc"],strings["off"])
 		ReadyCheckFrameYesButton:SetText(READY)
+		enabled = false
+	elseif msg == "login on" then
+		LazyReadyCheck.login = true
+		print(strings["lrc"],strings["onLogin"])
+	elseif msg == "login off" then
+		LazyReadyCheck.login = false
+		print(strings["lrc"],strings["offLogin"])
+	else
+		print(strings["lrc"],strings["onOff"])
+		print(strings["lrc"],strings["onOffLogin"])
 	end
 end
 
--- local toggle = CreateFrame("BUTTON","LazyReadyCheckToggle",ReadyCheckListenerFrame)
--- toggle:SetPoint("BOTTOMRIGHT", "ReadyCheckListenerFrame", "TOPRIGHT", -5, -13)
--- toggle:SetHeight(30)
--- toggle:SetWidth(30)
--- toggle:SetScript ("OnClick", function(self) enableAddon(not enabled) end)
-
--- local texture = toggle:CreateTexture(nil,"ARTWORK")
--- texture:SetTexture([[Interface\Icons\inv_misc_enggizmos_30]])
--- texture:SetAllPoints()
-
+local f = CreateFrame("Frame")
+f:RegisterEvent("PLAYER_LOGIN")
+f:SetScript("OnEvent", function(self,event,...)
+	local enabled = LazyReadyCheck.login
+	if enabled then
+		print(strings["lrc"],strings["options"])
+	else
+		print(strings["lrc"],strings["options"])
+	end
+end)
 
 ReadyCheckFrameYesButton:HookScript("OnUpdate",function(self,elapsed)
 	if enabled then
 		self.elapsed = self.elapsed - elapsed
-		self:SetFormattedText('%s (|cffffffff%.1f|r)', READY, self.elapsed)
+		self:SetFormattedText('%s |cffffffff(%.1f)|r', READY, self.elapsed)
 		if(self.elapsed <= 0) then
 			self:SetText(READY)
 			self:Click()
 		end
 	end
 end)
+
 ReadyCheckFrameYesButton:HookScript("OnShow", function(self)
-	self.elapsed = math.random(100,500)/10
+	self.elapsed = math.random(10,50)/10
 end)
-enableAddon(LazyReadyCheck.login)
-
-SLASH_LAZYREADYCHECK1 = "/lrc"
-SLASH_LAZYREADYCHECK2 = "/lazyreadycheck"
-SlashCmdList["LAZYREADYCHECK"] = function(msg, editbox)
-	-- enable/disable deprecated
-	if msg == "on" or msg == "enable" then
-		enableAddon(true)
-	elseif msg == "off" or msg == "disable" then
-		enableAddon(false)
-	elseif msg == "login on" then
-		LazyReadyCheck.login = true
-		print(stringLRC..[[|cff33CC33Turned on after login!|r! When logging in, addon will accept ready checks!]])
-	elseif msg == "login off" then
-		LazyReadyCheck.login = false
-		print(stringLRC..[[|cffFF0000Turned off after login|r! When logging in, addon will ignore ready checks!]])
-	else
-		print(stringLRC..[[/lrc on/off - autoaccept or ignore ready checks]])
-		print(stringLRC..[[/lrc login on/off- turn on Lazy Ready Check when logging in]])
-	end
-end
-
-
